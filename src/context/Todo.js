f59a7126caedf3ad2d2b7e577497411b412ff69c
Tharique Afridi <tharique@visiblestarsksa.com>
2023-09-19 12:31:27 +0300

    first push

---
+++ src/context/Todo.js
@@ -0,0 +1,24 @@
+export const todos = [
+    {
+        id:"1",
+        title:"Party"
+    },  
+    {
+        id:"2",
+        title:"Gym"
+    },
+    {
+        id:"3",
+        title:"Meeting"
+    },
+
+    {
+        id:"4",
+        title:"Walking"
+    },
+
+    {
+        id:"5",
+        title:"Coding"
+    },
+];
