462217168df68b54fd6b4185492109cfa5748e45
Thariqueafridi <126147044+Thariqueafridi@users.noreply.github.com>
2023-09-19 12:36:28 +0300

    Updated from var to const

--- src/components/TodoInput.jsx
+++ src/components/TodoInput.jsx
@@ -3,7 +3,7 @@ import { TodoContext } from '../context/TodoProvider'
 import { v1 } from 'uuid';
 function TodoInput() {
     const [name, setName] = useState("")
-    var todoContext = useContext(TodoContext);
+    const todoContext = useContext(TodoContext);
     return (
         <div className="row">
             <input value={name} onChange={(e) => setName(e.target.value)} placeholder="Add List" className="col form-control" type="text" />
